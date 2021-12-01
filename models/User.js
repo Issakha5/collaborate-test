17655f768b48d2ab78772a52bfa5bca75e2ea363
Issakha Diallo <issakha@opensystechnologies.com>
2021-12-01 13:54:36 +0000

    collaborate test

---
+++ models/User.js
@@ -0,0 +1,25 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const userModel = {
+   user_name: {
+      type: String,
+      required: true
+   },
+   email: {
+      type: String,
+      required: true
+   },
+   role: {
+    type: String,
+    default: 'basic',
+    enum: ["basic", "supervisor", "admin"]
+   },
+   password: {
+      type: String,
+      required: true
+   }
+};
+
+const UserSchema = new Schema(userModel);
+module.exports = mongoose.model("users", UserSchema);
